4fd68f01b5236d9c858153bbeb14a834301077b0
camijavi <camijavinavarro@gmail.com>
2026-04-26 17:00:46 -0600

    implement FiveNumberSum program to calculate the sum of five user-provided integers

--- FiveNumberSum/Program.cs
+++ FiveNumberSum/Program.cs
@@ -4,17 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-       int sum = 0;
-       
-       Console.WriteLine("Enter 5 numbers:");
-       for (int i = 0; i < 5; i++){
-        if (int.TryParse(Console.ReadLine(), out int number)){
-        sum += number;
-        }else{
-            Console.WriteLine("Invalid input. Please try again.");
-            i--;
+        int sum = 0;
+        Console.WriteLine("Enter 5 numbers:");
+
+        for (int i = 0; i < 5; i++)
+        {
+            if (int.TryParse(Console.ReadLine(), out int number))
+            {
+                sum += number;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+                
+            }
         }
-       }
+
         Console.WriteLine("The sum of the numbers is: " + sum);
+        Console.WriteLine("-------------------------");
     }
 }
