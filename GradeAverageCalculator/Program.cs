62acdb0b9c03c4c0311befc9f1076e83be230fba
camijavi <camijavinavarro@gmail.com>
2026-04-26 17:22:55 -0600

    implement grade average calculator console application

--- GradeAverageCalculator/Program.cs
+++ GradeAverageCalculator/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int n = 0;
+        int sum = 0;
+        double average = 0.0;
+
+        Console.Clear();
+        Console.WriteLine("==========================================");
+        Console.WriteLine("         GRADE AVERAGE CALCULATOR         ");
+        Console.WriteLine("==========================================");
+
+        while (true)
+        {
+            Console.Write("\nEnter the number of grades you want to input (or type 'x' to exit): ");
+         
+
+            string? input = Console.ReadLine()?.ToLower();
+
+            if (input == "x") break; //exits the loop if the user enters 'x';
+
+            if (int.TryParse(input, out n) && n > 0)
+            {
+                Console.WriteLine("Enter the grades:");
+                for (int i = 0; i < n; i++)
+                {
+                    if (int.TryParse(Console.ReadLine(), out int number) && number >= 0 && number <=100)
+                    {
+                        sum += number;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter a number between 0 and 100.");
+                        Console.ReadLine();
+                        
+                    }
+                }
+
+                average = (double)sum / n;
+
+                Console.WriteLine("\nThe average of the grades is: " + average.ToString("n2"));
+            }
+        }
+
+        Console.WriteLine("\nGoodbye!\nmade by @camijavi");
     }
 }
+
