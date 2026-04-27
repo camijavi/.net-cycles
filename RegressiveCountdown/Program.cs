5680b79681f5b798a226a563958d424721356199
camijavi <camijavinavarro@gmail.com>
2026-04-26 18:31:39 -0600

    implement regressive countdown console application

--- RegressiveCountdown/Program.cs
+++ RegressiveCountdown/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int num = 0;
+
+        Console.WriteLine("Enter a number to count down from:");
+
+        if (int.TryParse(Console.ReadLine(), out num) && num > 0)
+        {
+            Console.Clear();
+            for (int i = num; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        else { 
+            Console.WriteLine("Please enter a valid positive integer.");
+        }
     }
 }
