64d043ae39cd06c01d931c58dc61ca724dbe70ba
camijavi <camijavinavarro@gmail.com>
2026-04-26 20:29:28 -0600

    add age validation program and password-protected security portal console applications

--- PositiveAgevalidator/Program.cs
+++ PositiveAgevalidator/Program.cs
@@ -7,26 +7,25 @@ class Program
         
         while (true)
         {
-           Console.WriteLine("Please input your age:");
-
+           Console.WriteLine("\nPlease input your age:");
 
             string input = Console.ReadLine();
 
             if (!int.TryParse(input, out int age))
             {
-                Console.WriteLine("Error: You must enter a valid number (no text).");
+                Console.WriteLine("\nError: You must enter a valid number (no text).");
             }
             else if (age <= 0)
             {
-                Console.WriteLine("Error: Age must be greater than zero.");
+                Console.WriteLine("\nError: Age must be greater than zero.");
             }
             else
             {
-                Console.WriteLine($"Your age is: {age}");
+                Console.WriteLine($"\nYour age is: {age}");
                 break;
             }
         }  
 
-        Console.WriteLine("\nGoodbye!\n Made by @camijavi");
+        Console.WriteLine("\nGoodbye!\nMade by @camijavi");
     }
 }
--- SecurityPortal/Program.cs
+++ SecurityPortal/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string password = "camila123";
+        string userInput = "";
+
+        while(true){
+            Console.Write("Please enter your password:");
+            userInput = Console.ReadLine();
+
+            if(userInput == password){
+                Console.WriteLine("Access granted!");
+                
+            }
+            else{
+                Console.WriteLine("Access DENIED! Try again....");
+            }
+        }
+ 
     }
 }
