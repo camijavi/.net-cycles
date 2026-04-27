c64d62d22a2a7834892bdfa48bcc4bf24165e45a
camijavi <camijavinavarro@gmail.com>
2026-04-26 19:17:33 -0600

    add program to calculate sum of even numbers up to user input

--- SumEvenToN/Program.cs
+++ SumEvenToN/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int sum = 0;
+
+
+        Console.WriteLine("Enter an even number:");
+        if (int.TryParse(Console.ReadLine(), out int evenNumber))
+        {
+            for (int i = 1; i <= evenNumber; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+        }
+        
+        Console.WriteLine($"Sum of even numbers: {sum}");
     }
 }
